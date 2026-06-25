9e8572d8509df787a252b96eb1b702a97a7893b0
Federico Morsucci <fedem3867@gmail.com>
2026-06-25 17:46:38 +0200

    first desing of Domain

--- src/main/scala/soundcode/domain/Domain.scala
+++ src/main/scala/soundcode/domain/Domain.scala
@@ -1,3 +1,29 @@
 package soundcode.domain
 
-case class Note(name: String, duration: Int)
+case class Stream(base: Pattern, extensions: List[Pattern])
+
+type Pattern= List[Seq[Element]]
+
+enum Element:
+  case Sound
+  case AggregationPattern
+  case Effect
+
+enum Sound:
+  case NoteInText
+  case SampleInText
+
+case class NoteInText(nota:Note,position: TextPosition)
+case class SampleInText(sample:Sample,position: TextPosition)
+case class TextPosition(startIndex: Int, endIndex:Int)
+
+enum AggregationPattern:
+  case SubPattern(pattern: Pattern)
+  case AlternationPattern(pattern: Pattern)
+
+enum Effect:
+  case Gain(value: Int)
+  case Room(value: Int)
+ 
+type Note = String
+type Sample = String
