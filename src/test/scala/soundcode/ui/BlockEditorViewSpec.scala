10848d2e99e6f1d87c6d7f7a3138cf73148d7817
Giacomo Biagioni <biagioni11@gmail.com>
2026-06-27 00:25:18 +0200

    fix gui tests

--- src/test/scala/soundcode/ui/BlockEditorViewSpec.scala
+++ src/test/scala/soundcode/ui/BlockEditorViewSpec.scala
@@ -6,54 +6,62 @@ import javafx.scene.control.Label
 import javafx.scene.layout.{HBox, VBox}
 import javafx.scene.input.{KeyEvent, KeyCode}
 import soundcode.ui.editor.BlockEditorView
+import org.scalatest.BeforeAndAfterAll
 
-class BlockEditorViewSpec extends UITestSupport:
+class BlockEditorViewSpec extends UITestSupport with BeforeAndAfterAll:
   private val initialCode =
-    """note("c4 e4 g4").sound("piano").slow(2)
-      |sound("bd sn hh").bank("tr909")
-      |note("a3 c4 e4").sound("saw").gain(0.7)
-      |stack(note("c5").sound("pluck"), sound("hh*8"))""".stripMargin
+    "note(\"c4 e4 g4\").sound(\"piano\").slow(2)\n" +
+      "sound(\"bd sn hh\").bank(\"tr909\")\n" +
+      "note(\"a3 c4 e4\").sound(\"saw\").gain(0.7)\n" +
+      "stack(note(\"c5\").sound(\"pluck\"), sound(\"hh*8\"))"
+
+  private def editorWithInitialCode(): BlockEditorView =
+    val editor = new BlockEditorView
+    editor.renderCode(initialCode)
+    editor
+
+  private def blocksBoxOf(editor: BlockEditorView): VBox =
+    editor.root.content.value.asInstanceOf[VBox]
+
+  private def lineNumberAt(editor: BlockEditorView, index: Int): Label =
+    rowAt(editor, index).getChildren.get(0).asInstanceOf[Label]
+
+  private def rowAt(editor: BlockEditorView, index: Int): HBox =
+    val blocksBox = editor.root.content.value.asInstanceOf[VBox]
+    val block = blocksBox.getChildren.get(index).asInstanceOf[VBox]
+    block.getChildren.get(0).asInstanceOf[HBox]
 
   private def lineEditorAt(
       editor: BlockEditorView,
       index: Int
   ): InlineCssTextArea =
-    val blocksBox = editor.root.content.value.asInstanceOf[VBox]
-    val row = blocksBox.getChildren.get(index).asInstanceOf[HBox]
-    row.getChildren.get(1).asInstanceOf[InlineCssTextArea]
+    rowAt(editor, index).getChildren.get(1).asInstanceOf[InlineCssTextArea]
 
   test("block editor exposes the current code without changing it"):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
+      val editor = editorWithInitialCode()
+      val actual = editor.currentCode
 
-      assert(editor.currentCode == initialCode)
+      assert(actual == initialCode)
 
   test("block editor creates one visual row for each line of code"):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
-      val blocksBox = editor.root.content.value.asInstanceOf[VBox]
+      val editor = editorWithInitialCode()
+      val blocksBox = blocksBoxOf(editor)
 
       assert(blocksBox.getChildren.size == 4)
 
   test("block editor shows progressive line numbers"):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
-      val blocksBox = editor.root.content.value.asInstanceOf[VBox]
+      val editor = editorWithInitialCode()
 
-      val firstRow = blocksBox.getChildren.get(0).asInstanceOf[HBox]
-      val fourthRow = blocksBox.getChildren.get(3).asInstanceOf[HBox]
-
-      val firstLabel = firstRow.getChildren.get(0).asInstanceOf[Label]
-      val fourthLabel = fourthRow.getChildren.get(0).asInstanceOf[Label]
-
-      assert(firstLabel.getText == "1")
-      assert(fourthLabel.getText == "4")
+      assert(lineNumberAt(editor, 0).getText == "1")
+      assert(lineNumberAt(editor, 3).getText == "4")
 
   test("line rows use the configured visual height"):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
-      val blocksBox = editor.root.content.value.asInstanceOf[VBox]
-      val row = blocksBox.getChildren.get(0).asInstanceOf[HBox]
+      val editor = editorWithInitialCode()
+      val row = rowAt(editor, 0)
 
       assert(row.getPrefHeight == 34)
       assert(row.getMinHeight == 34)
@@ -61,36 +69,18 @@ class BlockEditorViewSpec extends UITestSupport:
 
   test("line number column uses the same width as the line height"):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
-      val blocksBox = editor.root.content.value.asInstanceOf[VBox]
-      val row = blocksBox.getChildren.get(0).asInstanceOf[HBox]
-      val lineNumber = row.getChildren.get(0).asInstanceOf[Label]
+      val editor = editorWithInitialCode()
+      val lineNumber = lineNumberAt(editor, 0)
 
       assert(lineNumber.getPrefWidth == 34)
       assert(lineNumber.getMinWidth == 34)
       assert(lineNumber.getMaxWidth == 34)
 
-  test("changing a line editor notifies with the full current code"):
-    onFxThread:
-      var lastChange = ""
-
-      val editor = BlockEditorView(initialCode, code => lastChange = code)
-      val firstInlineCssTextArea = lineEditorAt(editor, 0)
-
-      firstInlineCssTextArea.replaceText("changed")
-
-      val expectedCode = initialCode
-        .split("\n", -1)
-        .updated(0, "changed")
-        .mkString("\n")
-
-      assert(lastChange == expectedCode)
-
   test(
     "arrow keys move focus between line editors preserving caret column when possible"
   ):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
+      val editor = editorWithInitialCode()
 
       val firstLine = lineEditorAt(editor, 0)
       val secondLine = lineEditorAt(editor, 1)
@@ -111,7 +101,7 @@ class BlockEditorViewSpec extends UITestSupport:
 
   test("line editor key codes split and merge lines"):
     onFxThread:
-      val editor = BlockEditorView(initialCode, _ => ())
+      val editor = editorWithInitialCode()
 
       val originalLines = initialCode.split("\n", -1).toSeq
 
--- src/test/scala/soundcode/ui/MainViewSpec.scala
+++ src/test/scala/soundcode/ui/MainViewSpec.scala
@@ -1,8 +1,10 @@
 package soundcode.ui
 
+import soundcode.mvu.Msg
+
 class MainViewSpec extends UITestSupport:
   test("main view has a root"):
     onFxThread:
-      val view = MainView()
+      val view = MainView(_ => ())
 
       assert(view.root != null)
