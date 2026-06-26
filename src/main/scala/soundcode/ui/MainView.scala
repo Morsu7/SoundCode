a39eee61cbc571a271d2a8f628b6847154fed265
Giacomo Biagioni <biagioni11@gmail.com>
2026-06-27 00:47:35 +0200

    remove warning in mainview

--- src/main/scala/soundcode/ui/MainView.scala
+++ src/main/scala/soundcode/ui/MainView.scala
@@ -10,6 +10,7 @@ import javafx.event.{ActionEvent, EventHandler}
 import soundcode.ui.editor.BlockEditorView
 import soundcode.mvu.Msg
 import soundcode.mvu.AppModel
+import scala.annotation.nowarn
 
 class MainView(
     dispatch: Msg => Unit
@@ -24,6 +25,8 @@ class MainView(
     center = editorView.root
     top = toolbar
 
+  // to remove this nowarn we should update the scalfx version to a version that supports scala 3
+  @nowarn("msg=Implicit parameters should be provided with a `using` clause")
   private def toolbar: ToolBar =
     new ToolBar:
       content = Seq(
